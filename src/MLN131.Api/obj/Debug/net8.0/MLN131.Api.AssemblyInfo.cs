f047382216e6212e1d71786a2a3544cfd9c125fe
PHẠM MINH NHỰT <nhutpham0303@gmail.com>
2026-03-10 16:15:04 +0700

    cập nhật API

--- src/MLN131.Api/obj/Debug/net8.0/MLN131.Api.AssemblyInfo.cs
+++ src/MLN131.Api/obj/Debug/net8.0/MLN131.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLN131.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a916a87039dfdf64ae5408a2a45550be3e329c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLN131.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLN131.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
